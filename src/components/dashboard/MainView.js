81ac8a39dccaac70488202bcdfa03ca344af41fb
Bojan2111 <bojan.adzic@gmail.com>
2023-09-18 10:56:19 +0200

    Edit AddEmployee component with fake data

--- src/components/dashboard/MainView.js
+++ src/components/dashboard/MainView.js
@@ -1,15 +1,14 @@
 import React from "react";
 import classes from "./MainView.module.css";
 import PersonalSchedule from "../views/schedule/PersonalSchedule";
+import AddEmployee from "../views/employee/AddEmployee";
 
 const MainView = (props) => {
   return (
     <div className={classes.container}>
-      <h1>{props.content.title}</h1>
-      <h3>{props.content.subtitle}</h3>
-      <p>{props.content.info}</p>
-      <h2>{props.feature}</h2>
-      {props.feature === "Licni" ? <PersonalSchedule /> : <p>Nothing</p>}
+      {/* <h2>{props.feature}</h2>
+      {props.feature === "Licni" ? <PersonalSchedule /> : <p>Nothing</p>} */}
+      <AddEmployee />
     </div>
   );
 };
--- src/components/views/employee/AddEmployee.js
+++ src/components/views/employee/AddEmployee.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./AddEmployee.module.css";
 
-/*
-        public int YearOfEmployment { get; set; }
-        public string LicenseNumber { get; set; }
-        public int? ContractTypeId { get; set; }
-        public int? PositionId { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
-        public DateTime ReligiousHoliday { get; set; }
-*/
 const AddEmployee = () => {
+  // const [contractTypes, setContractTypes] = useState([]);
+  // const [positions, setPositions] = useState([]);
+
+  // TODO: fetch for getting positons and contract types.
+
+  const usernameRef = useRef("");
+  const passwordRef = useRef("");
+  const firstNameRef = useRef("");
+  const lastNameRef = useRef("");
+  const dateOfBirthRef = useRef("");
+  const emailRef = useRef("");
+  const yearOfEmploymentRef = useRef("");
+  const licenseNumberRef = useRef("");
+  const contractTypeRef = useRef("");
+  const positionRef = useRef("");
+  const addressRef = useRef("");
+  const cityRef = useRef("");
+  const zipCodeRef = useRef("");
+  const religiousHolidayRef = useRef("");
+
+  const contractTypes = ["Određeno", "Neodređeno"];
+  const positions = [
+    "Organizaciona sestra",
+    "Edukator",
+    "Medicinska sestra / tehničar",
+  ];
+
+  const handleSubmitData = (event) => {
+    event.preventDefault();
+
+    // could add validation here...
+
+    const data = {
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+      email: emailRef.current.value,
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+      dateOfBirth: dateOfBirthRef.current.value,
+      yearOfEmployment: parseInt(yearOfEmploymentRef.current.value),
+      licenseNumber: licenseNumberRef.current.value,
+      contractTypeId: parseInt(contractTypeRef.current.value),
+      positionId: parseInt(positionRef.current.value),
+      address: addressRef.current.value,
+      city: cityRef.current.value,
+      zipCode: zipCodeRef.current.value,
+      religiousHoliday: religiousHolidayRef.current.value,
+    };
+
+    console.log(data);
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.header}>
         <h2>Add New</h2>
         <p>Some description</p>
       </div>
-      <form>
+      <form onSubmit={handleSubmitData}>
         <div className={classes["input-field"]}>
           <label htmlFor="username">Username</label>
           <input
@@ -26,6 +68,7 @@ const AddEmployee = () => {
             id="username"
             type="text"
             placeholder="Username"
+            ref={usernameRef}
           />
         </div>
         <div className={classes["input-field"]}>
@@ -35,6 +78,7 @@ const AddEmployee = () => {
             id="firstName"
             type="text"
             placeholder="First Name"
+            ref={firstNameRef}
           />
         </div>
         <div className={classes["input-field"]}>
@@ -44,11 +88,18 @@ const AddEmployee = () => {
             id="lastName"
             type="text"
             placeholder="Last Name"
+            ref={lastNameRef}
           />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="email">Email</label>
-          <input name="email" id="email" type="email" placeholder="email" />
+          <input
+            name="email"
+            id="email"
+            type="email"
+            placeholder="email"
+            ref={emailRef}
+          />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="password">Password</label>
@@ -57,15 +108,26 @@ const AddEmployee = () => {
             id="password"
             type="password"
             placeholder="Password"
+            ref={passwordRef}
           />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="dateOfBirth">Date Of Birth</label>
-          <input name="dateOfBirth" id="dateOfBirth" type="date" />
+          <input
+            name="dateOfBirth"
+            id="dateOfBirth"
+            type="date"
+            ref={dateOfBirthRef}
+          />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="yearOfEmployment">YearOfEmployment</label>
-          <input name="yearOfEmployment" id="yearOfEmployment" type="number" />
+          <input
+            name="yearOfEmployment"
+            id="yearOfEmployment"
+            type="number"
+            ref={yearOfEmploymentRef}
+          />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="licenseNumber">License Number</label>
@@ -74,18 +136,33 @@ const AddEmployee = () => {
             id="licenseNumber"
             type="text"
             placeholder="License Number"
+            ref={licenseNumberRef}
           />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="contractType">Contract Type</label>
-          <select>
-            <option></option>
+          <select ref={contractTypeRef}>
+            {contractTypes.map((ct) => (
+              <option
+                key={ct.substring(0, 3)}
+                value={`${contractTypes.indexOf(ct) + 1}`}
+              >
+                {ct}
+              </option>
+            ))}
           </select>
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="position">Position</label>
-          <select>
-            <option></option>
+          <select ref={positionRef}>
+            {positions.map((p) => (
+              <option
+                key={p.substring(0, 3)}
+                value={`${positions.indexOf(p) + 1}`}
+              >
+                {p}
+              </option>
+            ))}
           </select>
         </div>
         <div className={classes["input-field"]}>
@@ -95,11 +172,18 @@ const AddEmployee = () => {
             id="address"
             type="text"
             placeholder="address"
+            ref={addressRef}
           />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="city">City</label>
-          <input name="city" id="city" type="text" placeholder="city" />
+          <input
+            name="city"
+            id="city"
+            type="text"
+            placeholder="city"
+            ref={cityRef}
+          />
         </div>
         <div className={classes["input-field"]}>
           <label htmlFor="zipCode">ZIP Code</label>
@@ -108,13 +192,24 @@ const AddEmployee = () => {
             id="zipCode"
             type="text"
             placeholder="zipCode"
+            ref={zipCodeRef}
           />
         </div>
         <div className={classes["input-field"]}>
-          <label htmlFor="religiousHoliday">Date Of Birth</label>
-          <input name="religiousHoliday" id="religiousHoliday" type="date" />
+          <label htmlFor="religiousHoliday">Religious Holiday</label>
+          <input
+            name="religiousHoliday"
+            id="religiousHoliday"
+            type="date"
+            ref={religiousHolidayRef}
+          />
+        </div>
+        <div className={classes.submit}>
+          <button>Submit</button>
         </div>
       </form>
     </div>
   );
 };
+
+export default AddEmployee;
