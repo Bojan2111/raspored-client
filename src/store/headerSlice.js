ee57e7f9aece28357d59c2b43f281aacd2ae997b
Bojan2111 <bojan.adzic@gmail.com>
2023-10-15 21:31:09 +0200

    Edit header slice

--- src/store/headerSlice.js
+++ src/store/headerSlice.js
@@ -3,15 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const headerSlice = createSlice({
   name: "header",
   initialState: {
+    optionsList: [],
     selectedOptionId: 1,
   },
   reducers: {
     setSelectedOption: (state, action) => {
       state.selectedOptionId = action.payload;
     },
+    setOptionsList: (state, action) => {
+      state.optionsList = action.payload;
+    },
   },
 });
 
-export const { setSelectedOption } = headerSlice.actions;
+export const { setSelectedOption, setOptionsList } = headerSlice.actions;
 
 export default headerSlice.reducer;
